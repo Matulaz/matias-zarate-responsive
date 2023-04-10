e5dd0a0ce83cb3dc457c2bd01ea71e84ba8bcaeb
matulaz <matiaszarate.psi@gmail.com>
2023-04-10 17:45:54 -0300

    botonera desktop

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {  ChakraProvider, Box } from '@chakra-ui/react'
 import MobileNav from './components/mobile/MobileNav'
 import DesktopNav from './components/desktop/DesktopNav'
 import DesktopHero from './components/desktop/DesktopHero'
+import DesktopBotonera from './components/desktop/DesktopBotonera'
 
 
 function App() {
@@ -11,12 +12,13 @@ function App() {
 
   return (
     <ChakraProvider>
-      <Box display={{base:'flex', md:'none'}}>
+      <Box display={{base:'inline', md:'none'}}>
         <MobileNav />
       </Box>
-      <Box display={{base:'none', md:'flex'}}>
+      <Box display={{base:'none', md:'inline'}}>
         <DesktopNav />
         <DesktopHero />
+        <DesktopBotonera />
       </Box>
     </ChakraProvider>
   )
---
+++ src/components/desktop/DesktopBotonera.jsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import { Box, ChakraProvider, Flex } from '@chakra-ui/react'
+import './DesktopBotonera.css'
+
+
+
+
+function DesktopBotonera() {
+
+
+
+
+    return(
+        <ChakraProvider>
+        <Flex justify='center' >
+            <Box className='Buttons-container' display='flex' alignItems='center'>
+                    <Box p='30px' ps='20'>portfolio dev.</Box>
+                    <Box p='30px'borderLeft='#E8E2E2 solid 2px'>logros academicos</Box>
+                    <Box p='30px' pe='20' borderLeft='#E8E2E2 solid 2px'>acerca de mi...</Box>
+            </Box>
+        </Flex>
+    </ChakraProvider>
+    )
+}
+
+export default DesktopBotonera;
