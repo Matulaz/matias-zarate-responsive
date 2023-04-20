0bf9ba9c418bd3b563f733fe2a8e615ccbbb3297
matulaz <matiaszarate.psi@gmail.com>
2023-04-19 23:55:11 -0300

    estructura de seccion proyectos

--- src/components/desktop/DesktopProyectos.jsx
+++ src/components/desktop/DesktopProyectos.jsx
@@ -1,7 +1,9 @@
 import * as React from 'react'
-import {  ChakraProvider, Box, Text } from '@chakra-ui/react'
+import {  ChakraProvider, Box, Text, Image, Flex } from '@chakra-ui/react'
 import './DesktopProyectos.css'
-
+import notebook from '../../assets/img/notebook.svg'
+import nyc from '../../assets/img/nyc.jpg'
+import sfo from '../../assets/img/sfo.jpg'
 
 
 function DesktopProyectos(){
@@ -9,10 +11,23 @@ function DesktopProyectos(){
     return(
         <ChakraProvider>
             <Box className="Proyectos">
-            <Box pl='200px' >
+                <Box pl='200px' >
                         <Text py='10px' fontSize='1.2em'fontWeight={600}>Acerca de mis...</Text> 
-                        <Text py='10px' color='red' fontSize='2.2em' letterSpacing={1} fontWeight={600} h='1000'>Proyectos</Text>
+                        <Text py='10px' color='red' fontSize='2.2em' letterSpacing={1} fontWeight={600} >Proyectos</Text>
                 </Box>
+
+                <Flex justifyContent='center' alignItems='center'>
+                    
+                    <Image boxSize='sm' src={notebook} w='50%'/>
+                
+                    <Flex justifyContent='center' alignItems='center' w='50%' wrap='wrap' direction='row' paddingRight='100' >
+                        <Image boxSize='250' src={nyc} m='4'/>
+                        <Image boxSize='250' src={sfo} m='4'/>
+                        <Image boxSize='250' src={sfo} m='4'/>
+                        <Image boxSize='250' src={nyc} m='4'/>
+                    </Flex>
+
+                </Flex>
 
             </Box>
         </ChakraProvider>       
